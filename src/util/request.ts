b551ca0d9597d04f3bac34aab4038da3136e5053
jvms1998 <jvms9817@gmail.com>
2025-03-04 19:48:57 -0300

    parcial

---
+++ src/util/request.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export type RequestConfig = AxiosRequestConfig;
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Response<T = any> extends AxiosResponse<T> {}
+
+export class Request {
+
+  constructor(private request = axios) { }
+
+  public get<T>(url: string, config: RequestConfig = {}): Promise<Response<T>> {
+    return this.request.get<T, Response<T>>(url, config);
+  }
+}
